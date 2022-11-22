09a26d0d520026e3cc248a15c39710f882a8defc
André Kanashiro <alhkanashiro@gmail.com>
2022-11-22 12:32:54 -0300

    Add atualizacao

--- Frontend/FRONT_FilmesDB/src/app/models/filme.ts
+++
@@ -1,9 +0,0 @@
-import { Genero } from "./genero";
-
-export interface Filme {
-  id?: number;
-  nome: string;
-  ano: number;
-  generoId: number;
-  genero?: Genero;
- }
--- Frontend/FRONT_FilmesDB/src/app/models/genero.ts
+++
@@ -1,4 +0,0 @@
-export interface Genero {
-    generoId?: number;
-    nome: string;
-}
